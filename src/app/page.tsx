b5e61a260025175bb917d2dcd047a592bdc2c24c
andrewprog97 <andrewprog97@gmail.com>
2024-01-13 02:40:40 +0300

    add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-
+import styles from "@/styles/Home.module.scss";
+import Image from "next/image";
+import logoImg from "@/assets/UI/logo.png";
 
 export default function Home() {
     return (
-        <p>Hello world</p>
+        <>
+            <div className={styles.home__wrapper}>
+                <div className={styles.home}>
+                    <Image 
+                        src={logoImg}
+                        alt="drawguess"
+                        className={styles.home__logo}
+                    />
+                    <h1>DrawGuess.ru</h1>
+                </div>
+            </div>
+        </>
     );
 }
