13ebe2ec6e029f0b51ae6edf650ab89c0ddc37f3
andrewprog97 <andrewprog97@gmail.com>
2024-01-13 02:59:50 +0300

    add home page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import styles from "@/styles/Home.module.scss";
 import Image from "next/image";
 import logoImg from "@/assets/UI/logo.png";
+import Input from "@/components/Input/Input";
 
 export default function Home() {
     return (
@@ -13,6 +14,10 @@ export default function Home() {
                         className={styles.home__logo}
                     />
                     <h1>DrawGuess.ru</h1>
+                    <Input
+                        className={styles.home__input}
+                        placeholder="Твой ник"
+                    />
                 </div>
             </div>
         </>
---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,11 @@
+import styles from "./Input.module.scss";
+
+export default function Input(props: React.HTMLProps<HTMLInputElement>) {
+    const { className, ...restProps } = props;
+    return (
+        <input 
+            className={`${styles.input} ${props.className}`}
+            {...restProps}
+        />
+    )
+}
