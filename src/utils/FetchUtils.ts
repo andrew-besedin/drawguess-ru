b468a1a1a44dd6668df82b5c4a1809470e390b57
andrewprog97 <andrewprog97@gmail.com>
2024-01-16 20:50:06 +0300

    add fetch utils

---
+++ src/utils/FetchUtils.ts
@@ -0,0 +1,8 @@
+
+// Backend fetches are planned to be created here.
+// Potentially axios will be added in future.
+export default class FetchUtils {
+    // static async fetchAnything() {
+    //     return await fetch("...").then(res => res.json());
+    // }
+}
