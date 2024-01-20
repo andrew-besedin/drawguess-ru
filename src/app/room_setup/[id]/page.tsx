de9cfd63e272636eedbac01f2c4a0c802fefd4de
andrewprog97 <andrewprog97@gmail.com>
2024-01-20 05:08:20 +0300

    add room setup page content

--- src/app/room_setup/[id]/page.tsx
+++ src/app/room_setup/[id]/page.tsx
@@ -1,10 +1,26 @@
+import styles from "@/styles/RoomSetup.module.scss";
 import BackButton from "@/components/default/BackButton/BackButton";
 import Link from "next/link";
-
+import Input from "@/components/UI/Input/Input";
+import Button from "@/components/UI/Button/Button";
 
 export default function RoomSetup() {
     return (
         <>
+            <main className={styles["room-setup__wrapper"]}>
+                <div className={styles["room-setup"]}>
+                    <div className={styles["room-setup__settings"]}>
+                        <div className={styles["room-setup__settings-list"]}></div>
+                        <Input />
+                    </div>
+                    <div className={styles["room-setup__players"]}>
+                        <div className={styles["room-setup__players-list"]}>
+
+                        </div>
+                        <Button>Играть</Button>
+                    </div>
+                </div>
+            </main>
             <Link href="/">
                 <BackButton />
             </Link>
